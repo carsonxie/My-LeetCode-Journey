0a3b39a3ae78130218db2b18b3509331c93487bd
carsonxie <carsontj@outlook.com>
2020-06-16 10:05:33 -0600

    Create 1_twoSum.py

---
+++ code/1_twoSum.py
@@ -0,0 +1,13 @@
+class Solution: 
+        
+    def twoSum(self, nums, target):
+        #new method use dict
+        
+        dict = {}
+        
+        for i in range(len(nums)):
+            if target - nums[i] not in dict:
+                dict[nums[i]] = i   #key is number, value is index
+            else:
+                return [dict[target-nums[i]],i]
+        
